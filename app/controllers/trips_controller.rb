18f48161733d5eb3b3c06cbd36b3b5bed36075c8
Allegra <aukseallegra@gmail.com>
2018-10-04 03:00:51 +0100

    edited controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,19 +1,33 @@
 class TripsController < ApplicationController
+  before_action :set_trip, only: [:show, :edit, :update, :destroy]
 
   def index
-    trips = Trip.all
-    render json: trips
+    @trips = Trip.all
+    render json: @trips
   end
 
+  # POST /trips
   def create
-    trip = Trip.create(trips_params)
-    render json: trip
+    @trip = Trip.create(trips_params)
+    render json: @trip
   end
 
-  def update
-    trip = trip.update(trips_params)
-    render json: trip
+  # GET /trips/1
+  def show
   end
+
+  # GET /trips/1/edit
+  def edit
+  end
+
+  # PATCH/PUT /trips/1
+  def update
+    # byebug   #have the right info here
+    @trip = @trip.update(trips_params)
+    render json: @trip
+  end
+
+  # DELETE /trips/1
 
 # PATCH/PUT /lists/1
 # def update
@@ -42,8 +56,12 @@ class TripsController < ApplicationController
 
   private
 
+  def set_trip
+    @trip = Trip.find(params[:id])
+  end
+
   def trips_params
-    params.require(:trip).permit(:name, :country, :location, :things_did,
+    params.require(:trip).permit(:id, :name, :country, :location, :things_did,
     :notes, :date_from, :date_to, :user_id)
   end
 
